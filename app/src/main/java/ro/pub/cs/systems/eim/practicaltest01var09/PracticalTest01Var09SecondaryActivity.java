ec018fb436dcdee73eedded3e94997bb1e0c967a
LeonardCiobanu <leoncbn@yahoo.com>
2025-11-13 14:57:58 +0200

    B.2

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var09/PracticalTest01Var09SecondaryActivity.java
@@ -0,0 +1,25 @@
+package ro.pub.cs.systems.eim.practicaltest01var09;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PracticalTest01Var09SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_practical_test01_var09_secondary);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
